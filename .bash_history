182c97bfd9325e25a1c5d48f1ee5ddd14e2ea09c
dennis <dennis@partnerhero.com>
2017-11-17 18:38:17 +0000

    commit after installing slack sdk

---
+++ .bash_history
@@ -0,0 +1,93 @@
+ls
+cd ..
+lsl
+ls
+cd ~
+node --version
+cler
+clear
+npm install http-server -g
+vim --version
+clear
+mkdir page
+cd page
+touch index.html
+vim index.html
+http-server .
+http-server ./
+clear
+sudo apt-get update
+apt-get install nginx
+ufw app list
+ufw allow 'Nginx HTTP'
+ufw status
+systemctl status nginx
+vim /etc/nginx/sites-available/default
+nginx -t
+systemctl restart nginx
+http-server ./
+vim /etc/nginx/sites-available/default
+vim /var/log/nginx/error.log
+http-server ./
+vim ./index.html 
+http-server ./
+npm install -g forever
+forever start http-server ./
+forever -help
+forever --help
+forever -h
+forever start -c http-server
+clear
+ls
+cd page
+ls
+cat index.html
+cd ..
+ls
+ls -a
+cat .git/config
+git --help
+git --info-path
+git --branch
+git branch
+git log
+git status
+git push
+echo "# xmqapp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git config --global user.name dennis
+git config --global user.email dennis@partnerhero.com
+git commit -m "first commit"
+git remote add origin https://github.com/dennischiangb/xmqapp.git
+git push -u origin master
+cat ~/.ssh/id_rsa.pub
+git push -u origin master
+ls ~/.ssh/*.pub
+git push -u origin master
+git remote add origin https://github.com/dennischiangb/xmqapp
+git push -u origin master
+git push -u origin
+git push -u origin master
+git config
+.git/config
+cd .git/config
+git config blob
+git config path
+git config --path
+cat .git/config
+git remote add origin https://github.com/dennischiangb/xmqapp
+git --help
+git init
+git add README.md
+git commit -m "first commit?"
+nano .git/config
+cat .git/config
+git init
+echo "# xmqapp" >> README.md
+git init
+git add README.md
+git commit -m "first commit??"
+git push -u origin master
+cat ~/.ssh/id_rsa.pub
---
+++ index.js
@@ -0,0 +1,56 @@
+// Enable strict mode, this allows us to use ES6 specific syntax
+// such as 'const' and 'let'
+'use strict';
+
+// Import the Real Time Messaging (RTM) client
+// from the Slack API in node_modules
+const RtmClient = require('@slack/client').RtmClient;
+
+// The memory data store is a collection of useful functions we // can
+// include in our RtmClient
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+
+// Import the RTM event constants from the Slack API
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+// Import the client event constants from the Slack API
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+const token = '';
+
+// The Slack constructor takes 2 arguments:
+// token - String representation of the Slack token
+// opts - Objects with options for our implementation
+let slack = new RtmClient(token, {
+  // Sets the level of logging we require
+  logLevel: 'debug', 
+  // Initialize a data store for our client, this will 
+  // load additional helper functions for the storing 
+  // and retrieval of data
+  dataStore: new MemoryDataStore(),
+  // Boolean indicating whether Slack should automatically 
+  // reconnect after an error response
+  autoReconnect: true,
+  // Boolean indicating whether each message should be marked as // read 
+  // or not after it is processed 
+  autoMark: true 
+});
+
+// Add an event listener for the RTM_CONNECTION_OPENED 
+// event, which is called 
+// when the bot connects to a channel. The Slack API can 
+// subscribe to events by using the 'on' method
+slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+  // Get the user's name
+  let user = slack.dataStore.getUserById(slack.activeUserId);
+
+  // Get the team's name
+  let team = slack.dataStore.getTeamById(slack.activeTeamId);
+
+  // Log the slack team name and the bot's name, using ES6's // template
+  // string syntax
+  console.log(`Connected to ${team.name} as ${user.name}`);
+});
+
+// Start the login process
+slack.start();
